7b32dc7f8adac8da2b8944096a420c8646b1367b
Hubert Lin <linhub15@gmail.com>
2018-09-17 10:32:01 -0600

    Add Example

--- es5/dom.js
+++ es5/dom.js
@@ -1,3 +1,10 @@
+/**
+ * Create an HTML Element with attributes and children nodes
+ *
+ * @param tag The HTML tag to be created
+ * @param attrs Nullable, supply HTML Attributes as key value pairs {class: 'panel', id: 'submitBtn', href: 'https://example.com'}
+ * @param children HTML Elements or text. DO NOT use JSX <p>Text</p>
+ */
 function el(tag, attrs) {
     var children = [];
     for (var _i = 2; _i < arguments.length; _i++) {
@@ -15,3 +22,4 @@ function el(tag, attrs) {
     }
     return element;
 }
+var example = el('div', { "id": 'main', "class": 'panel' }, el('h1', null, 'My Example'), el('p', null, 'Using this function sure makes creating DOM elements easier'));
--- lib/dom.ts
+++ lib/dom.ts
@@ -1,13 +1,22 @@
+/**
+ * Create an HTML Element with attributes and children nodes
+ * 
+ * @param tag The HTML tag to be created
+ * @param attrs Nullable, supply HTML Attributes as key value pairs {class: 'panel', id: 'submitBtn', href: 'https://example.com'}
+ * @param children HTML Elements or text. DO NOT use JSX <p>Text</p>
+ */
 function el(tag, attrs, ...children) {
-    let element = tag ? document.createElement(tag) : null;
-    let keys = attrs && element ? Object.keys(attrs) : null;
-    let childNodes = children && element ? children : null;
-    for (let i in keys) {
-      element.setAttribute(keys[i], attrs[keys[i]]);
-    }
-    for (let node of childNodes) {
-      element.appendChild(
-        node instanceof HTMLElement ? node : document.createTextNode(node));
-    }
-    return element;
+  let element = tag ? document.createElement(tag) : null;
+  let keys = attrs && element ? Object.keys(attrs) : null;
+  let childNodes = children && element ? children : null;
+  for (let i in keys) {
+    element.setAttribute(keys[i], attrs[keys[i]]);
   }
+  for (let node of childNodes) {
+    element.appendChild(
+      node instanceof HTMLElement ? node : document.createTextNode(node));
+  }
+  return element;
+}
+
+var example = el('div', {"id":'main', "class": 'panel'}, el('h1',null,'My Example'), el('p',null,'Using this function sure makes creating DOM elements easier'));
