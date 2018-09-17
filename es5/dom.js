5cc672c3af8e1aca469ba99c191be1529bb24c67
Hubert Lin <linhub15@gmail.com>
2018-09-17 10:34:01 -0600

    Add output example

--- es5/dom.js
+++ es5/dom.js
@@ -23,3 +23,10 @@ function el(tag, attrs) {
     return element;
 }
 var example = el('div', { "id": 'main', "class": 'panel' }, el('h1', null, 'My Example'), el('p', null, 'Using this function sure makes creating DOM elements easier'));
+/**
+ * Output:
+ * <div id="main" class="panel">
+ *    <h1>My Example</h1>
+ *    <p>Using this function sure makes creating DOM elements easier</p>
+ * </div>
+ */ 
